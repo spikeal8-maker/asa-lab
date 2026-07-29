46f4683a8d2919029d744476308e580b03473671
Alex Al <spikeal8@gmail.com>
2026-07-30 01:13:24 +0300

    feat(electronics): export persisted breadboard attachment contract

--- contexts/electronics/index.ts
+++ contexts/electronics/index.ts
@@ -116,6 +116,7 @@ export {
 export {
   EMPTY_DOCUMENT,
   parseElectronicsDocument,
+  type BreadboardAttachment,
   type ComponentPosition,
   type DocumentParseResult,
   type ElectronicsDocument,
